20c08caffa23aa2a1f55196edae7c4b6e4e4d28a
Andy <andy.decandia@gmail.com>
2018-01-28 02:55:34 +0100

    postgresql attempt

--- src/main/java/Game/Battleship/BattleshipApplication.java
+++ src/main/java/Game/Battleship/BattleshipApplication.java
@@ -2,6 +2,13 @@ package Game.Battleship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BattleshipApplication {
@@ -9,6 +16,16 @@ public class BattleshipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BattleshipApplication.class, args);
 		System.out.println("hi");
+	}
+
+	@Configuration
+	public class DatabaseConfig {
+		@Bean
+		@Primary
+		@ConfigurationProperties(prefix = "spring.datasource")
+		public DataSource dataSource() {
+			return DataSourceBuilder.create().build();
+		}
 	}
 }
 
